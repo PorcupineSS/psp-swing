06da9f36e4f0440dc2693118c7c2b2bce0d8c920
javergarav <javergarav@unal.edu.co>
2013-05-26 03:01:10 -0500

    + Creación de paquetes.

---
+++ PSP/src/main/java/com/porcupine/psp/model/entity/EntityClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.porcupine.psp.model.entity;
+
+/**
+ *
+ * @author andres
+ */
+public class EntityClass {
+    
+}

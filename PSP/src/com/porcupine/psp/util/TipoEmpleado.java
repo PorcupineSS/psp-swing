ed349d940f3b71e7757163d83c8d6d1c55054407
Sergio Cortés <sacortesh@gmail.com>
2013-06-09 15:49:51 -0500

    Agregados enumeradores para tipo de empleado y tipo de telefono

---
+++ PSP/src/com/porcupine/psp/util/TipoEmpleado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.porcupine.psp.util;
+
+/**
+ *Enumeracion de los tipos de empleado que pueden existir segun un rol. Uso en el vo de Empleados recomendado.
+ * @author sparrow
+ */
+public enum TipoEmpleado {
+    
+    GENERICO, COORDINADOR_CONTRATO, COORDINADOR_TECNICO_TECNOLOGICO, DIRECTOR_COMERCIAL, DIRECTOR_GESTION_HUMANA, DIRECTOR_OPERACIONES, PLANTA, TEMPORAL, SUBGERENTE
+    
+    
+}
---
+++ PSP/src/com/porcupine/psp/util/TipoTelefono.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.porcupine.psp.util;
+
+/**
+ *Enumeracion de tipos de telefono para solo necesitar de un vo para crear todo y facilitar el trabajo
+ * @author sparrow
+ */
+public enum TipoTelefono {
+    
+    CLIENTE, EMPLEADO, PROVEEDOR
+    
+    
+}

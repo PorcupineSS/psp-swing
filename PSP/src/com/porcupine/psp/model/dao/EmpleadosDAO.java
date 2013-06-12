a0770bf4fb357b793e99ed01b3be698ad4a6a97b
Sergio Andrés Cortés Hernández <sacortesh@gmail.com>
2013-06-12 02:30:33 -0500

    Implementados todos los metodos en el dao

--- PSP/src/com/porcupine/psp/model/dao/EmpleadosDAO.java
+++ PSP/src/com/porcupine/psp/model/dao/EmpleadosDAO.java
@@ -4,9 +4,12 @@
  */
 package com.porcupine.psp.model.dao;
 
+import com.porcupine.psp.model.entity.DirGestionHum;
 import com.porcupine.psp.model.entity.Empleados;
 import java.util.List;
-import javax.persistence.EntityNotFoundException;
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaQuery;
+import model.dao.exceptions.DataBaseException;
 import model.dao.exceptions.NonexistentEntityException;
 import model.dao.exceptions.PreexistingEntityException;
 
@@ -14,37 +17,144 @@ import model.dao.exceptions.PreexistingEntityException;
  *
  * @author Zergio
  */
-public class EmpleadosDAO implements ICrudDAO<Empleados, Long>{
+public class EmpleadosDAO implements ICrudDAO<Empleados, Integer> {
+
+    private EntityManagerFactory entityManagerFactory;
 
     EmpleadosDAO(String PU) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        entityManagerFactory = Persistence.createEntityManagerFactory(PU);
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
     }
 
     @Override
     public void create(Empleados entity) throws PreexistingEntityException, NonexistentEntityException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            entityManager.getTransaction().begin();
+            entityManager.persist(entity);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
-    public Empleados find(Long id) throws EntityNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Empleados find(Integer id) throws EntityNotFoundException {
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            return entityManager.find(Empleados.class, id);
+        } catch (EntityNotFoundException ex) {
+            throw new EntityNotFoundException("El empleado con id " + id + " no existe.");
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void update(Empleados entity) throws NonexistentEntityException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            entityManager.getTransaction().begin();
+
+
+
+            entity = entityManager.merge(entity);
+
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager != null && entityManager.getTransaction() != null) {
+                entityManager.getTransaction().rollback();
+            }
+
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
 
     @Override
-    public void delete(Long id) throws NonexistentEntityException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void delete(Integer id) throws NonexistentEntityException {
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            entityManager.getTransaction().begin();
+            Empleados empleado = null;
+            try {
+                empleado = entityManager.getReference(Empleados.class, id);
+            } catch (EntityNotFoundException e) {
+                throw new NonexistentEntityException("El usuario con id " + id + " no existe.", e);
+            }
+
+            entityManager.remove(empleado);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager != null && entityManager.getTransaction() != null) {
+                entityManager.getTransaction().rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<Empleados> getList() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            CriteriaQuery cq = entityManager.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Empleados.class));
+            Query q = entityManager.createQuery(cq);
+            return q.getResultList();
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
     
-    
+    public Empleados login(Empleados entity) throws DataBaseException {
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            Empleados empleado;
+            Query q = entityManager.createQuery("SELECT u FROM Empleados u "
+                    + "WHERE u.coddocume LIKE :username "
+                    + "AND u.contrasenae LIKE :password")
+                    .setParameter("username", entity.getCoddocume())
+                    .setParameter("password", entity.getApellidoe());
+            try {
+                empleado = (Empleados) q.getSingleResult();
+            } catch (NoResultException e) {
+                empleado = null;
+            }
+            return empleado;
+        } catch (Exception e) {
+            throw new DataBaseException("Error de Conexion a la Base de Datos", e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
+    }
     
 }

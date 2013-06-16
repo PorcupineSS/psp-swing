1b5f38c46b61b14e7584f01c2fb0f868a1bd3a1f
javergarav <javergarav@unal.edu.co>
2013-06-16 17:30:19 -0500

    Implementado ImplSeguridadDAO.

--- PSP/src/com/porcupine/psp/model/dao/ImplSeguridadDAO.java
+++ PSP/src/com/porcupine/psp/model/dao/ImplSeguridadDAO.java
@@ -6,9 +6,14 @@ package com.porcupine.psp.model.dao;
 
 import com.porcupine.psp.model.dao.exceptions.NonexistentEntityException;
 import com.porcupine.psp.model.entity.ImplSeguridad;
-import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
 import model.dao.exceptions.PreexistingEntityException;
 
 /**
@@ -17,30 +22,111 @@ import model.dao.exceptions.PreexistingEntityException;
  */
 public class ImplSeguridadDAO implements ICrudDAO<ImplSeguridad, Integer> {
 
+    private EntityManagerFactory entityManagerFactory;
+
+    ImplSeguridadDAO(String PU, Map propierties) {
+        entityManagerFactory = Persistence.createEntityManagerFactory(PU, propierties);
+    }
+    
+    public EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+    
     @Override
     public void create(ImplSeguridad entity) throws PreexistingEntityException, NonexistentEntityException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            entityManager.getTransaction().begin();
+            entityManager.persist(entity);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public ImplSeguridad find(Integer id) throws EntityNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            return entityManager.find(ImplSeguridad.class, id);
+        } catch (EntityNotFoundException ex) {
+            throw new EntityNotFoundException("¡El implemento con id: " + id + ", no existe!");
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void update(ImplSeguridad entity) throws NonexistentEntityException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            entityManager.getTransaction().begin();
+            entity = entityManager.merge(entity);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager != null && entityManager.getTransaction() != null) {
+                entityManager.getTransaction().rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void delete(Integer id) throws NonexistentEntityException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            entityManager.getTransaction().begin();
+            ImplSeguridad implementoSeg = null;
+            try {
+                implementoSeg = entityManager.getReference(ImplSeguridad.class, id);
+            } catch (EntityNotFoundException e) {
+                throw new NonexistentEntityException("¡El implemento con id: " + id + ", no existe!", e);
+            }
+
+            entityManager.remove(implementoSeg);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager != null && entityManager.getTransaction() != null) {
+                entityManager.getTransaction().rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<ImplSeguridad> getList() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         EntityManager entityManager = null;
+        try {
+            entityManager = getEntityManager();
+            CriteriaQuery cq = entityManager.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(ImplSeguridad.class));
+            Query q = entityManager.createQuery(cq);
+            return q.getResultList();
+        } finally {
+            if (entityManager != null) {
+                entityManager.clear();
+                entityManager.close();
+            }
+        }
     }
-    
     
 }

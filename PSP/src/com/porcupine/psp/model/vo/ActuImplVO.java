ef4bb516899d20767d59f2dceaea904526ac379c
javergarav <javergarav@unal.edu.co>
2013-06-16 16:53:54 -0500

    Implementado ActuImplVO.

--- PSP/src/com/porcupine/psp/model/vo/ActuImplVO.java
+++ PSP/src/com/porcupine/psp/model/vo/ActuImplVO.java
@@ -4,10 +4,68 @@
  */
 package com.porcupine.psp.model.vo;
 
+import java.util.Date;
+
 /**
  *
  * @author Jeisson Andrés Vergara
  */
 public class ActuImplVO {
     
+    private static final long serialVersionUID = 1L;
+    
+    private Short idActualizacionI;
+    private Date fechaActualizacionI;
+    private Short cantidadAgregada;
+    private String descripcionActualizacion;
+    private Short idImplemento;
+    private Integer cedulaCoordTyT;
+
+    public Short getIdActualizacionI() {
+        return idActualizacionI;
+    }
+
+    public void setIdActualizacionI(Short idActualizacionI) {
+        this.idActualizacionI = idActualizacionI;
+    }
+
+    public Date getFechaActualizacionI() {
+        return fechaActualizacionI;
+    }
+
+    public void setFechaActualizacionI(Date fechaActualizacionI) {
+        this.fechaActualizacionI = fechaActualizacionI;
+    }
+
+    public Short getCantidadAgregada() {
+        return cantidadAgregada;
+    }
+
+    public void setCantidadAgregada(Short cantidadAgregada) {
+        this.cantidadAgregada = cantidadAgregada;
+    }
+
+    public String getDescripcionActualizacion() {
+        return descripcionActualizacion;
+    }
+
+    public void setDescripcionActualizacion(String descripcionActualizacion) {
+        this.descripcionActualizacion = descripcionActualizacion;
+    }
+
+    public Short getIdImplemento() {
+        return idImplemento;
+    }
+
+    public void setIdImplemento(Short idImplemento) {
+        this.idImplemento = idImplemento;
+    }
+
+    public Integer getCedulaCoordTyT() {
+        return cedulaCoordTyT;
+    }
+
+    public void setCedulaCoordTyT(Integer cedulaCoordTyT) {
+        this.cedulaCoordTyT = cedulaCoordTyT;
+    }
 }

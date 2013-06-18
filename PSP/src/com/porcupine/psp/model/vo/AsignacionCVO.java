da95f13fda5d2ab5f16955998741e0b7b262df8f
Duvan Lancheros <duvan1118@gmail.com>
2013-06-17 20:34:27 -0500

    Se termina AsignacionCVO, se crea EmpTempVO pues era necesaria para AsinacionVO pero esta parcialmente hecha...

--- PSP/src/com/porcupine/psp/model/vo/AsignacionCVO.java
+++ PSP/src/com/porcupine/psp/model/vo/AsignacionCVO.java
@@ -5,6 +5,8 @@
 package com.porcupine.psp.model.vo;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -14,6 +16,69 @@ public class AsignacionCVO implements IValueObject, Serializable{
     
     private static final long serialVersionUID = 1L;
     
+    private Short idAsig;
+    private Short idContrato;
+    private Integer cedulaEmpTemp;//tengo duda sobre si este si el nombre xD
+    private Integer cedulaCoordCont;
+    private Date fechaAsignacionC;
+    private String horarioAsignado;
     
+    private List<EmpTempVO> empTempList;
+
+    public Short getIdAsig() {
+        return idAsig;
+    }
+
+    public void setIdAsig(Short idAsig) {
+        this.idAsig = idAsig;
+    }
+
+    public Short getIdContrato() {
+        return idContrato;
+    }
+
+    public void setIdContrato(Short idContrato) {
+        this.idContrato = idContrato;
+    }
+
+    public Integer getCedulaEmpTemp() {
+        return cedulaEmpTemp;
+    }
+
+    public void setCedulaEmpTemp(Integer cedulaEmpTemp) {
+        this.cedulaEmpTemp = cedulaEmpTemp;
+    }
+
+    public Integer getCedulaCoordCont() {
+        return cedulaCoordCont;
+    }
+
+    public void setCedulaCoordCont(Integer cedulaCoordCont) {
+        this.cedulaCoordCont = cedulaCoordCont;
+    }
+
+    public Date getFechaAsignacionC() {
+        return fechaAsignacionC;
+    }
+
+    public void setFechaAsignacionC(Date fechaAsignacionC) {
+        this.fechaAsignacionC = fechaAsignacionC;
+    }
+
+    public String getHorarioAsignado() {
+        return horarioAsignado;
+    }
+
+    public void setHorarioAsignado(String horarioAsignado) {
+        this.horarioAsignado = horarioAsignado;
+    }
+
+    public List<EmpTempVO> getEmpTempList() {
+        return empTempList;
+    }
+
+    public void setEmpTempList(List<EmpTempVO> empTempList) {
+        this.empTempList = empTempList;
+    }
     
 }

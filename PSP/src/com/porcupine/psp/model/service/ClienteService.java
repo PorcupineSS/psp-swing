f323fb527a3bb15f7a7e215c0251f2992103391a
Johan David Rodriguez Portela <jodrodriguezpo@unal.edu.co>
2013-06-23 21:58:55 -0500

    Get list para servicio Cliente, aún hay dudas para los demás métodos de esta clase

--- PSP/src/com/porcupine/psp/model/service/ClienteService.java
+++ PSP/src/com/porcupine/psp/model/service/ClienteService.java
@@ -4,13 +4,19 @@
  */
 package com.porcupine.psp.model.service;
 
+import com.porcupine.psp.model.dao.DAOFactory;
 import com.porcupine.psp.model.dao.exceptions.InsufficientPermissionsException;
 import com.porcupine.psp.model.dao.exceptions.InvalidAttributeException;
 import com.porcupine.psp.model.dao.exceptions.NonexistentEntityException;
 import com.porcupine.psp.model.dao.exceptions.PreexistingEntityException;
 import com.porcupine.psp.model.dao.exceptions.RequiredAttributeException;
+import com.porcupine.psp.model.entity.Cliente;
+import com.porcupine.psp.model.entity.Contrato;
+import com.porcupine.psp.model.entity.DirComercial;
 import com.porcupine.psp.model.vo.ClienteVO;
+import com.porcupine.psp.model.vo.ContratoVO;
 import com.porcupine.psp.model.vo.EmpleadosVO;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityNotFoundException;
 
@@ -19,19 +25,33 @@ import javax.persistence.EntityNotFoundException;
  * @author Johan
  */
 public class ClienteService implements IService<ClienteVO, Integer> {
+
     private static ClienteService instance;
 
-        public static synchronized ClienteService getInstance() {
+    public static synchronized ClienteService getInstance() {
         if (instance == null) {
             instance = new ClienteService();
         }
         return instance;
     }
+
     
+    //sin terminar
     @Override
     public void create(ClienteVO vo) throws PreexistingEntityException, NonexistentEntityException, RequiredAttributeException, InvalidAttributeException, InsufficientPermissionsException {
+        Cliente entity = new Cliente();
 
-        
+        if (vo.getCedulaDirector() != null) {
+            DirComercial director = DAOFactory.getInstance().getDirComercialDAO().find((int) vo.getCedulaDirector());
+            entity.setCedulae(director);
+        }
+
+        List<ContratoVO> contratosVO = vo.getContratoList();
+        List<Contrato> contrato = new ArrayList<Contrato>();
+        Contrato contratoAdd = new Contrato();
+        for (ContratoVO contratoV : contratosVO) {
+        }
+
     }
 
     @Override
@@ -51,12 +71,17 @@ public class ClienteService implements IService<ClienteVO, Integer> {
 
     @Override
     public List<ClienteVO> getList() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<ClienteVO> listaVO = new ArrayList<ClienteVO>();
+        List<Cliente> lista = DAOFactory.getInstance().getClienteDAO().getList();;
+        for (Cliente cliente : lista) {
+            ClienteVO client = cliente.toVO();
+            listaVO.add(client);
+        }
+        return listaVO;
     }
 
     @Override
     public void removeAll() throws NonexistentEntityException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
 }
